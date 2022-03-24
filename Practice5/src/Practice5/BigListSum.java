ddf94ca3f22995cc292f76cddf76fc346a89c1d2
Ashutosh1103 <ashutosh.mulay@neosoftmail.com>
2022-03-25 02:50:06 +0530

    first commit

---
+++ Practice5/src/Practice5/BigListSum.java
@@ -0,0 +1,47 @@
+package Practice5;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Res {
+
+    
+    public static long aVeryBigSum(List<Long> ar) {
+    long sum=0;
+       for(int i=0;i<ar.size();i++){
+           sum = sum + ar.get(i);
+       }
+       return sum;
+    }
+
+}
+
+public class BigListSum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Long> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Long::parseLong)
+            .collect(toList());
+        
+        long result = Res.aVeryBigSum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
---
+++ Practice5/src/Practice5/NonRepeated.java
@@ -0,0 +1,15 @@
+package Practice5;
+
+public class NonRepeated {
+    public static void main(String args[]) {
+     
+        String inputStr ="teeter";
+
+        for(char i :inputStr.toCharArray()){
+        if ( inputStr.indexOf(i) == inputStr.lastIndexOf(i)) {
+            System.out.println("First non-repeating character is: "+i);
+            break;
+        }
+        }
+    }
+}
